d0d8896da33170dd5d9f0ba1dbaaf24530194640
VarvaraKrestyanova <varvara.krestyanova@spartasystems.com>
2022-06-01 18:04:01 +0300

    run type

--- src/main/java/helpers/WebDriverSingleton.java
+++ src/main/java/helpers/WebDriverSingleton.java
@@ -1,19 +1,27 @@
 package helpers;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class WebDriverSingleton {
 
     private static final String DRIVER_BROWSER = "driver.browser";
+    private static final String RUN_CONFIG = "run.config";
     private static String browserName = PropertiesUtil.get(DRIVER_BROWSER);
+    private static String runConfig = PropertiesUtil.get(RUN_CONFIG);
 
     private WebDriver driver;
     private static WebDriverSingleton instance;
+    private DesiredCapabilities desiredCapabilities;
 
     private WebDriverSingleton() {}
 
@@ -25,14 +33,27 @@ public class WebDriverSingleton {
     }
 
     public WebDriver getDriver() {
+        
         if (driver == null) {
-            switch (browserName) {
-                case "chrome":
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver(); break;
-                case "ff":
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver(); break;
+            if (runConfig == "locally") {
+                switch (browserName) {
+                    case "chrome":
+                        WebDriverManager.chromedriver().setup();
+                        driver = new ChromeDriver(); break;
+                    case "ff":
+                        WebDriverManager.firefoxdriver().setup();
+                        driver = new FirefoxDriver(); break;
+                }
+
+            } else if (runConfig == "grid") {
+                desiredCapabilities = new DesiredCapabilities();
+                desiredCapabilities.setBrowserName("chrome");
+                desiredCapabilities.setPlatform(Platform.WINDOWS);
+                try {
+                    driver = new RemoteWebDriver(new URL("http://192.168.100.25:4444/wd/hub"), desiredCapabilities);
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                }
             }
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         }
